0c4a1cc9b709f063a1b23d4f7808df96254f3ab1
Mike Vondrasek <mvondras@yahoo.com>
2023-10-03 10:50:16 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+
+package main
+
+import "fmt"
+
+func main() {
+        my_map := make(map[int]int)
+        my_map[2] = 4
+        my_map[4] = 16
+        my_map[8] = 64
+        delete(my_map, 4)
+        fmt.Print(my_map)
+}
